c8d6e552012e0072ca40a75daf5404ba19aff5cc
Lucas Poffo <lucas.poffo@magrathealabs.com>
2020-04-19 00:40:45 -0300

    Add new visual

--- Interfaces/Timer.gd
+++ Interfaces/Timer.gd
@@ -9,17 +9,19 @@ func _on_Timer_timeout():
 	_seconds -= 1
 	_updateLabel()
 	if _seconds == 0:
-		$Timer.stop()
-		Event.emit_signal("timer_expired")
+		_ampoleError()
+
+func _ampoleError():
+	Event.emit_signal("ampole_error")
+	$Timer.stop()
+	$TimerLabel.text = "--:--"
 
 func _updateLabel():
 	$TimerLabel.text = "00:%02d" % _seconds
-	
+
 func _on_Ampoule_pressed():
 	if _seconds <= 10:
 		_seconds = 60
 		_updateLabel()
 	else:
-		Event.emit_signal("ampole_error")
-		$Timer.stop()
-		$TimerLabel.text = "XX:XX"
+		_ampoleError()
