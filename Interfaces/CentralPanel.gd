3931a09c72aa3f51da22d6a31ff167a5f63f4f07
Lucas Poffo <lucas.poffo@magrathealabs.com>
2020-04-19 23:16:22 -0300

    Add sound to buttons

--- Interfaces/CentralPanel.gd
+++ Interfaces/CentralPanel.gd
@@ -7,18 +7,6 @@ func _ready():
 	$Label.text = String(v1)
 	$Label2.text = String(v2)
 
-func _on_TextureButton_pressed():
-	v1 += 1
-	if v1 > 9:
-		v1 = 0
-	$Label.text = String(v1)
-	
-func _on_TextureButton2_pressed():
-	v2 += 1
-	if v2 > 9:
-		v2 = 0
-	$Label2.text = String(v2)
-
 func set_triangle_disabled(disabled : bool):
 	$Submit.disabled = disabled
 	$Submit2.disabled = disabled
@@ -32,3 +20,15 @@ func _on_Submit_pressed():
 
 func _on_Binary_pressed():
 	Event.emit_signal("binary_submit", v1 * 10 + v2)
+
+func _on_Add_pressed():
+	v1 += 1
+	if v1 > 9:
+		v1 = 0
+	$Label.text = String(v1)
+
+func _on_Add2_pressed():
+	v2 += 1
+	if v2 > 9:
+		v2 = 0
+	$Label2.text = String(v2)
