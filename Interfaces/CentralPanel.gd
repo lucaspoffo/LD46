042203ef9f74a1232842c86032f4e9aeb8e5785d
Lucas Poffo <lucas.poffo@magrathealabs.com>
2020-04-19 21:46:26 -0300

    Add submit for all buttons, redo visual

--- Interfaces/CentralPanel.gd
+++ Interfaces/CentralPanel.gd
@@ -19,5 +19,16 @@ func _on_TextureButton2_pressed():
 		v2 = 0
 	$Label2.text = String(v2)
 
+func set_triangle_disabled(disabled : bool):
+	$Submit.disabled = disabled
+	$Submit2.disabled = disabled
+
+func set_binary_disabled(disabled : bool):
+	$Binary0.disabled = disabled
+	$Binary1.disabled = disabled
+
 func _on_Submit_pressed():
-	Event.emit_signal("central_panel_submit", v1 * 10 + v2)
+	Event.emit_signal("triangle_submit", v1 * 10 + v2)
+
+func _on_Binary_pressed():
+	Event.emit_signal("binary_submit", v1 * 10 + v2)
--- Main.gd
+++ Main.gd
@@ -8,21 +8,36 @@ var second_puzzle = 1
 var count = 0
 var puzzles_done = 0
 
+var fib_sequence = [0, 1, 1, 2, 3, 5, 8]
+var current_fib = 0
+
 func _ready():
 	Event.connect("ampoule_error", self, "restart_expirement")
 	Event.connect("timer_expired", self, "restart_expirement")
 	Event.connect("ampoule_injected", self, "_on_AmpouleInjected")
 	Event.connect("lightsout_puzzle_completed", self, "on_Puzzle_Completed")
 	Event.connect("switch_puzzle_completed", self, "on_Puzzle_Completed")
+	
+	Event.connect("binary_submit", self, "_on_BinarySubmit")
+	Event.connect("triangle_submit", self, "_on_TriangleSubmit")
+	
 	start_puzzle([first_puzzle])
 
 func restart_expirement():
 	$ButtonPuzzle.visible = false
 	$LightsOut.visible = false
+	
+	$FibSubmit.disabled = false
+	$FibSubmit2.disabled = false
+	
+	$CentralPanel.set_binary_disabled(false)
+	$CentralPanel.set_triangle_disabled(false)
+	
 	$ButtonPuzzle.init()
 	$LightsOut.init()
 	count = 0
 	puzzles_done = 0
+	current_fib = 0
 	if randi() % 2 == 1:
 		first_puzzle = 0
 		second_puzzle = 1
@@ -62,3 +77,27 @@ func on_Puzzle_Completed():
 	puzzles_done += 1
 	if count < 2 || puzzles_done == 2:
 		$Timer.set_injectable(true)
+		
+func _on_FibSubmit_pressed():
+	var value = $CentralPanel.v1 * 10 + $CentralPanel.v2
+	if value == fib_sequence[current_fib]:
+		current_fib += 1
+	else:
+		current_fib = 0
+		Event.emit_signal("error_submit")
+	if current_fib == fib_sequence.size():
+		$FibSubmit.disabled = true
+		$FibSubmit2.disabled = true
+		print("Fibonacci sequence submited")
+
+func _on_TriangleSubmit(value):
+	if value == 13:
+		$CentralPanel.set_triangle_disabled(true)
+	else:
+		Event.emit_signal("error_submit")
+	
+func _on_BinarySubmit(value):
+	if value == 27:
+		$CentralPanel.set_binary_disabled(true)
+	else:
+		Event.emit_signal("error_submit")
